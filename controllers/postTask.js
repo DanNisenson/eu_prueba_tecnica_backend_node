89259b6f7556ed569245e82cdc439c7992f9ffee
DanNisenson <danpablo1@gmail.com>
2023-05-08 14:12:45 +0200

    👷Minor changes to postTask

--- controllers/postTask.js
+++ controllers/postTask.js
@@ -2,24 +2,24 @@ const fs = require("fs");
 const uuid = require("uuid");
 
 const postTask = (req, res) => {
-  const toDoList = JSON.parse(fs.readFileSync("db.json", "utf8"));
+  let toDoList = JSON.parse(fs.readFileSync("db.json", "utf8"));
   const task = {
     id: toDoList.length + 1,
     uuid: uuid.v4(),
     ...req.body,
   };
 
-  toDoList.push(task);
+  toDoList = [...toDoList, task];
 
   try {
     fs.writeFileSync("db.json", JSON.stringify(toDoList));
   } catch (error) {
     const errMsg =
       "There's been an error processing your request. Try again later.";
-    res.status(500).json(errMsg);
+    return res.status(500).json(errMsg);
   }
 
-  res.status(201).json(task);
+  return res.status(201).json(task);
 };
 
 module.exports = postTask;
