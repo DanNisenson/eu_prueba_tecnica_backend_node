b98d28568fbe05520da2470654d8e628f3084c40
DanNisenson <danpablo1@gmail.com>
2023-05-09 10:00:07 +0200

    🗄Add "cors" lib

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+var cors = require("cors");
 const path = require("path");
 const logger = require("morgan");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
